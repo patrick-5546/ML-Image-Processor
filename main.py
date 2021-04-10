acd17911d5c25b8a4bf2d25e515349d9dcd3163d
clee422 <64577745+clee422@users.noreply.github.com>
2021-04-09 20:40:06 -0700

    Web application now supports downloading images when run locally

--- main.py
+++ main.py
@@ -1,43 +1,53 @@
-import imghdr
+from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory
 import os
-from flask import Flask, request, render_template, redirect, url_for, abort, \
-    send_from_directory
-from werkzeug.utils import secure_filename
+import zipfile
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png']
-app.config['UPLOAD_PATH'] = 'uploads'
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.jpeg', '.png']
+app.config['UPLOAD_PATH'] = '/tmp'
 
 
-def validate_image(stream):
-    header = stream.read(512)
-    stream.seek(0)
-    format = imghdr.what(None, header)
-    if not format:
-        return None
-    return '.' + (format if format != 'jpeg' else 'jpg')
+@app.errorhandler(400)
+def invalid_type(e):
+    return "File type not a .png or .jpg", 400
 
-@app.route("/")
+
+@app.route('/')
 def index():
     files = os.listdir(app.config['UPLOAD_PATH'])
-    return render_template("main.html", files=files)
+    return render_template('main.html', files=files)
+
 
 @app.route('/', methods=['POST'])
 def upload_files():
     uploaded_file = request.files['file']
-    filename = secure_filename(uploaded_file.filename)
-    if filename != '':
-        file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
-                file_ext != validate_image(uploaded_file.stream):
-            abort(400)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-    return redirect(url_for('index'))
+    extensions = tuple(app.config['UPLOAD_EXTENSIONS'])
+    if uploaded_file.filename != '' and uploaded_file.filename.endswith(extensions):
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], uploaded_file.filename))
+    elif not uploaded_file.filename.endswith(extensions):
+        print(uploaded_file.filename + " was not image file!")
+        abort(400)
+    return redirect((url_for('index')))
 
-@app.route('/uploads/<filename>')
-def upload(filename):
-    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+@app.route('/download', methods=['GET'])
+def zip_and_download():
+    path = app.config['UPLOAD_PATH']
+    ziph = zipfile.ZipFile('Photos.zip', 'w', zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file),
+                       os.path.relpath(os.path.join(root, file),
+                                       os.path.join(path, '..')))
+    ziph.close()
+    return send_from_directory(directory=os.path.dirname(app.instance_path), filename="Photos.zip")
+
 
 if __name__ == "__main__":
+    app.config['UPLOAD_PATH'] = 'uploads'
+
+    old_files = [f for f in os.listdir('uploads')]
+    for f in old_files:
+        os.remove(os.path.join('uploads', f))
+
     app.run(host="127.0.0.1", port=8080, debug=True)
