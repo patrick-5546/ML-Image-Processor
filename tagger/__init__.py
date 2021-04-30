35247bffcc6c0ed7494f296d9c6b25dd5905187a
LuuO <LuuO2000@outlook.com>
2021-04-30 17:51:15 +0800

    Update tagger  - APIs to read/edit tags

--- tagger/__init__.py
+++ tagger/__init__.py
@@ -4,13 +4,13 @@ based on the output of the pretrained YOLOv5s model.
 """
 
 from collections.abc import Callable, Iterable
-import os, pyexiv2, torch
+import os, torch
 import time
 
 from .object_detection import ObjectDetection # pylint: disable=import-error
 from .face_detection import cropFaces # pylint: disable=import-error
 from .face_recognition import FaceRecognition # pylint: disable=import-error
-from .image_tag import ImageTag # pylint: disable=import-error
+from .image_tag import ImageLibrary, ImageTag # pylint: disable=import-error
 
 def initialize(workingDirectory: str, face_sample_folder: str):
     """
@@ -20,74 +20,54 @@ def initialize(workingDirectory: str, face_sample_folder: str):
         workingDirectory (str): path of the image folder
         face_sample_folder (str): path of the folder containing labeled face images
     """
-    global __mediator, __holder
+    global __mediator, __library
     __mediator = __MLModelMediator(face_sample_folder)
-    __holder = __ImageHolder(workingDirectory)
+    __library = ImageLibrary(workingDirectory)
 
 def tag():
     """
     Tags all images in the image folder. Requires the tagger to be initialized.
     """
     start = time.time()
-    __holder.scan()
-    __mediator.addTags(__holder.getImageList())
-    __holder.saveTagsToFile()
+    __library.scan()
+    __mediator.addTags(__library.getImageList())
+    __library.saveTagsToFile()
     print(f"tag() runtime: {time.time() - start} seconds")
 
-class __ImageHolder:
+def getTags(filename: str):
+    """
+    Read tags of an image. Run object detection and facial recognition if necessary.
 
-    def __init__(self, workingDirectory: str):
-        if not workingDirectory.endswith(('/', '\\')):
-            workingDirectory += '/'
-        if not os.path.isdir(workingDirectory):
-            raise OSError(f"Could not find \"{workingDirectory}\"")
-        self.__workingDirectory = workingDirectory
-        self.__imageFileExt = ('.jpeg', '.jpg', '.png')
-        self.images = dict()
+    Args:
+        filename (str): name of the image file
 
-    def __addImage(self, filename):
-        path = self.__workingDirectory + filename
-        if not os.path.exists(path):
-            raise FileNotFoundError(f"{path} does not exists")
-        if filename not in self.images:
-            self.images[filename] = ImageTag(path)
+    Returns:
+        tags: tags of the image
+    """
+    imgTag = __library.getItem(filename)
+    __mediator.addTags([imgTag])
+    return imgTag.getAllTags()
 
-    def scan(self):
-        start = time.time()
-        self.images = dict()
+def excludeTag(filename, tag):
+    """
+    Exclude a tag from an image. The excluded tag will no longer 
+    appear unless it is explicitly added back to the image.
 
-        files = os.listdir(self.__workingDirectory)
-        files = filter(lambda fn: fn.endswith(self.__imageFileExt), files)
-
-        for f in files:
-            self.__addImage(f)
-
-        count = len(self.images)
-        print(f'Found {count} image{"s" * (count > 1)} in \"{self.__workingDirectory}\"')
-        print(f"scan(self) runtime: {time.time() - start} seconds")
-
-    def saveTagsToFile(self):
-        start = time.time()
-        for image in self.images.values():
-            tagImage(image.filepath, image.getAllTags())
-        print(f"saveTagsToFile(self) runtime: {time.time() - start} seconds")
+    Args:
+        filename (str): name of the image file
+        tag (str): tag to exclude
+    """
+    __library.getItem(filename).excludeTag(tag)
     
-    def getImageList(self):
-        return list(self.images.values())
+def addTag(filename, tag):
+    """
+    Add a tag to an image. 
 
-    def excludeTag(self, filename, tag):
-        self.__addImage(filename)
-        self.images[filename].excludeTag(tag)
-    
-    def addTag(self, filename, tag):
-        self.__addImage(filename)
-        self.images[filename].addUserTag(tag)
-
-    def getTags(self, filename):
-        self.__addImage(filename)
-        imgTag = self.images[filename]
-        __mediator.addTags([imgTag])
-        return imgTag.getAllTags()
+    Args:
+        filename (str): name of the image file
+        tag (str): tag to add
+    """
+    __library.getItem(filename).addUserTag(tag)
     
 class __MLModelMediator:
 
@@ -127,27 +107,3 @@ class __MLModelMediator:
             image.setFaceTags(names)
         
         print(f"addTags Facial Recognition runtime: {time.time() - start} seconds")
-
-def tagImage(filepath: str, newTags: Iterable[str]):
-    """
-    Tag one image
-
-    Args:
-        filepath (str): the filepath of the image to tag
-        newTags (list[str]): new tags to add to the image
-    """
-    tagEntries = ['Xmp.dc.subject', 'Iptc.Application2.Keywords'] # Exif.Image.XPKeywords is not supported
-
-    print(f'Image: {filepath}, adding tags: {newTags}')
-    img = pyexiv2.Image(filepath)
-    read = {'Xmp': img.read_xmp, 'Iptc': img.read_iptc, 'Exif': img.read_exif}
-    modify = {'Xmp': img.modify_xmp, 'Iptc': img.modify_iptc, 'Exif': img.modify_exif}
-
-    for entry in tagEntries:
-        entryType = entry.split('.', 1)[0]
-        metadata = read[entryType]()
-        tags = set(newTags)
-        if entry in metadata:
-            tags.update(metadata[entry])
-        metadata[entry] = list(tags)
-        modify[entryType](metadata)
--- tagger/image_tag.py
+++ tagger/image_tag.py
@@ -1,4 +1,47 @@
+import os, time, pyexiv2
 from collections.abc import Callable, Iterable
+
+class ImageLibrary:
+    def __init__(self, workingDirectory: str):
+        if not workingDirectory.endswith(('/', '\\')):
+            workingDirectory += '/'
+        if not os.path.isdir(workingDirectory):
+            raise OSError(f"Could not find \"{workingDirectory}\"")
+        self.__workingDirectory = workingDirectory
+        self.__imageFileExt = ('.jpeg', '.jpg', '.png')
+        self.images = dict()
+
+    def __addImage(self, filename):
+        path = self.__workingDirectory + filename
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"{path} does not exists")
+        if filename not in self.images:
+            self.images[filename] = ImageTag(path)
+        else:
+            print(f"skiping {filename}")
+
+    def scan(self):
+        files = os.listdir(self.__workingDirectory)
+        files = filter(lambda fn: fn.endswith(self.__imageFileExt), files)
+
+        for f in files:
+            self.__addImage(f)
+
+        count = len(self.images)
+        print(f'Found {count} image{"s" * (count > 1)} in \"{self.__workingDirectory}\"')
+
+    def saveTagsToFile(self):
+        start = time.time()
+        for image in self.images.values():
+            tagImage(image.filepath, image.getAllTags())
+        print(f"saveTagsToFile(self) runtime: {time.time() - start} seconds")
+    
+    def getImageList(self):
+        return list(self.images.values())
+
+    def getItem(self, filename):
+        self.__addImage(filename)
+        return self.images[filename]
 
 class ImageTag:
     def __init__(self, filepath: str, objectTags: Iterable = set(), faceTags: Iterable = set()):
@@ -15,6 +58,7 @@ class ImageTag:
         return tags - self.__excludeTags
     
     def excludeTag(self, tag):
+        self.__addedUserTags.discard(tag)
         self.__excludeTags.add(tag)
     
     def addUserTag(self, tag):
@@ -43,3 +87,27 @@ class ImageTag:
 
     def isFaceRecognized(self):
         return self.__faceRecognized
+
+def tagImage(filepath: str, newTags: Iterable[str]):
+    """
+    Tag one image
+
+    Args:
+        filepath (str): the filepath of the image to tag
+        newTags (list[str]): new tags to add to the image
+    """
+    tagEntries = ['Xmp.dc.subject', 'Iptc.Application2.Keywords'] # Exif.Image.XPKeywords is not supported
+
+    print(f'Image: {filepath}, adding tags: {newTags}')
+    img = pyexiv2.Image(filepath)
+    read = {'Xmp': img.read_xmp, 'Iptc': img.read_iptc, 'Exif': img.read_exif}
+    modify = {'Xmp': img.modify_xmp, 'Iptc': img.modify_iptc, 'Exif': img.modify_exif}
+
+    for entry in tagEntries:
+        entryType = entry.split('.', 1)[0]
+        metadata = read[entryType]()
+        tags = set(newTags)
+        if entry in metadata:
+            tags.update(metadata[entry])
+        metadata[entry] = list(tags)
+        modify[entryType](metadata)
