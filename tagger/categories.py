476f4954d2efdec7b2187acfc0f130c7b7febf30
LuuO <LuuO2000@outlook.com>
2021-05-15 10:32:52 +0800

    cleanups

--- tagger/categories.py
+++ tagger/categories.py
@@ -1,12 +1,13 @@
-'''
+"""
 Mapping objects to the category tag they represent
-'''
-portrait = ['person','handbag','tie']
-group_photo = ['person','handbag','tie']
-urban = ['skyscraper','car', 'motorcycle','bus','truck','traffic light','fire hydrant', 'stop sign', 'parking meter',
-         'bench']
-pet = ['cat','dog']
-nature = ['tree', 'flower', 'bird',  'horse', 'sheep', 'cow','elephant', 'bear', 'zebra', 'giraffe', 'boat']
+"""
+
+portrait = ['person', 'handbag', 'tie']
+group_photo = ['person', 'handbag', 'tie']
+urban = ['skyscraper', 'car', 'motorcycle', 'bus', 'truck', 'traffic light', 'fire hydrant', 'stop sign',
+         'parking meter', 'bench']
+pet = ['cat', 'dog']
+nature = ['tree', 'flower', 'bird',  'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'boat']
 sports = ['bicycle', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
           'skateboard', 'surfboard', 'tennis racket']
 food = ['plate', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'bottle', 'wine glass', 'cup', 'sandwich',
